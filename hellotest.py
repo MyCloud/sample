d868e763c379caa3138445120dd59e55ea8e149a
menno <menno@MSC-Client-M12>
2011-12-12 19:36:45 +0100

    eerste test

---
+++ hellotest.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 11, 2011
+
+@author: menno
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 6
+    return y +a
+    
+print add(1,2)
+print addFixedValue(1)
