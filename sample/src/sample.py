e605274ce0dafacd099d705c180b511c8ca607bd
MyCloud <msc@go2mycloud.net>
2011-12-14 07:43:36 +0100

    init

---
+++ sample/src/sample.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 14, 2011
+
+@author: menno
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 11
+    return y +a
+    
+print add(1,2)
+print addFixedValue(1)
